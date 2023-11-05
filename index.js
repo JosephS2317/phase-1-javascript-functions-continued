86da81f0245bd207776da29b27c7ac86ada41a43
Joseph <JosephSped23@gmail.com>
2023-11-04 21:36:03 -0400

    Completes functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(action = 'roller-skate'){
+    return (`This Saturday, I want to ${action}!`);
+    console.log(`This Saturday, I want to ${action}!`); 
+}
+function mondayWork(activity= 'go to the office'){
+    return (`This Monday, I will ${activity}.`);
+    console.log(`This Monday, I will ${activity}.`); 
+}
+function wrapAdjective(symbol){
+    return function(adjective){
+        return "You are " + symbol + adjective + symbol + '!';
+    };
+     
+}
+
+   
+
